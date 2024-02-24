8a81f63599e0d4dcacf5e04b07e2d9bf931908c9
zczqxc5 <146037962+zczqxc5@users.noreply.github.com>
2024-02-24 17:30:19 +0000

    change

--- two_servo_rotation/two_servo_rotation.ino
+++ two_servo_rotation/two_servo_rotation.ino
@@ -5,6 +5,7 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_LiquidCrystal.h>
 #include <Servo.h>
+#include <EEPROM.h>
 #include "arduino_secrets.h"
 
 #define SENSOR_PIN A3
@@ -12,6 +13,20 @@
 Adafruit_LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 Servo servo;
 Servo servo1;
+
+int pos;
+int maxPulse = 2500;
+int minPulse = 500;
+int delayTime = 15;
+float db;
+const int eepromAddress = 0;  
+unsigned long previousMillis = 0;  // random number time
+unsigned long moveMillis = 0;      // servo move time
+unsigned long dbCheckMillis = 0; 
+const long interval = 1000;        // per 1s generate 1 random number
+const int moveInterval = 15;       // per 0.015s move servo
+const long dbInterval = 10000;  
+bool increasing = true;
 
 //set Wifi
 const char* ssid = SECRET_SSID;
@@ -30,90 +45,90 @@ float mapFloat(float x, float in_min, float in_max, float out_min, float out_max
 
 void setup() {
   Serial.begin(115200);
-  startWifi();
+  //startWifi();
 
   //Set pins
   pinMode(SENSOR_PIN, INPUT);
 
-  servo.attach(A0);
-  servo1.attach(A1);
-  
-  servo.writeMicroseconds(1480);
 
+  servo.attach(A0, 500, 2500);  //caibrate servo to PWM signal 500-2500μs
+  servo1.attach(A1, 500, 2500);
+    
   //LCD dispkay
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.setBacklight(HIGH);
   lcd.clear();
 }
 
 void loop() {
-  
-  // Calculate noise sensor value to db
-  int sensorValue = analogRead(SENSOR_PIN);
-  float voltage = sensorValue * (5.0 / 1023.0); // map sensor voltage value range from 0~1023 to 0~5
-  float mappedVoltage = mapFloat(voltage, 0.0, 5.0, 0.6, 2.6); // map sensor voltage value range from 0~5 to 0.6~2.6
-  float db = mapFloat(mappedVoltage, 0.6, 2.6, 30.0, 130.0); // map sensor voltage value from 0.6~2.6V to 30~130db
+  //avoid block
+  unsigned long currentMillis = millis();
 
-  //lcd print
-  lcd.setCursor(0, 0);
-  lcd.print("Loudness: ");
-  Serial.println("db:");
-  Serial.println(db);
-  lcd.print(db, 1);
-  lcd.print("dB");
+  //get db value every 10s 
+  if (currentMillis - dbCheckMillis >= dbInterval) {
+    dbCheckMillis = currentMillis;
+    float db = calculateDb(); 
+    //lcd print
+    lcd.setCursor(0, 0);
+    lcd.print("Loudness: ");
+    Serial.println("db:");
+    Serial.println(db);
+    lcd.print(db, 1);
+    lcd.print("dB");
+  }
 
-  //rotation function
-  if (db < 58) {
+  //upper servo rotation function
+  if (db < 40) {
     lcd.setCursor(0, 1);
     lcd.print("Status: Rest");
-    servo.writeMicroseconds(1402);// servo mode: stopped
-    delay(3840);
-    servo.writeMicroseconds(1483);
-    delay(2000);
-    servo.writeMicroseconds(1563);
-    delay(3840);
-    servo.writeMicroseconds(1483);
-    delay(2000);
-  } else if (db >= 58 && db < 59) {
+    //SERVO FUNCTION: stop
+    servo1.writeMicroseconds(1500);  //servo stop
+  } else if (db >= 40 && db < 59) {
     lcd.setCursor(0, 1);
     lcd.print("Status: Work");
-    servo.write(100);
-    servo1.write(180);
-    delay(6000);
-    servo.write(90);
-    servo1.write(90);  //
-    delay(1000);       //
-
-    servo.write(80);
-    servo1.write(0);  //
-    delay(6000);
-
-    servo.write(90);
-    servo1.write(90);
-    delay(1000);
-  }
-  else if (db >= 59) {
+    //SERVO Mode: circle rotation
+    int speed = map(db,40,59,1300,1000);
+    servo1.writeMicroseconds(speed);
+  } else if (db >= 59) {
     lcd.setCursor(0, 1);
     lcd.print("Status: Noisy");
-    // servo mode: random
-    servo.write(100);
-    delay(6000);
-    servo.write(90);
-    delay(1000);
-    servo.write(80);
-    delay(6000);
-    int randomAngle = random(0, 180);
-    servo1.write(randomAngle);
-    delay(30);
+    //servo mode: random
+    // generate random number per second to contrao servo1
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      int randomNumber = random(500, 2501);
+      Serial.println(randomNumber);
+      servo1.writeMicroseconds(randomNumber);
+    }
   }
-  readapi();
+
+   // Move servo gradually without blocking
+    if (currentMillis - moveMillis >= moveInterval) {
+      moveMillis = currentMillis;
+      servo.writeMicroseconds(pos);
+      if (increasing) {
+        pos += 2;
+        if (pos >= 2500) increasing = false;
+      } else {
+        pos -= 2;
+        if (pos <= 500) increasing = true;
+      }
+  }
 }
 
-
+//function for vlotage calculate
 float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+float calculateDb() {
+ // Calculate noise sensor value to db
+  int sensorValue = analogRead(SENSOR_PIN);
+  float voltage = sensorValue * (5.0 / 1023.0);                 // map sensor voltage value range from 0~1023 to 0~5
+  float mappedVoltage = mapFloat(voltage, 0.0, 5.0, 0.6, 2.6);  // map sensor voltage value range from 0~5 to 0.6~2.6
+  db = mapFloat(mappedVoltage, 0.6, 2.6, 30.0, 130.0);    // map sensor voltage value from 0.6~2.6V to 30~130db
+  return db;
+}
 
 void startWifi() {
   // We start by connecting to a WiFi network
@@ -134,42 +149,41 @@ void startWifi() {
 }
 
 void readapi() {
-  if (WiFi.status() == WL_CONNECTED) {
-   
-        
-    String lat = "51.6116402309989";               
-    String lon = "-0.12889632674983237";               
+    if (WiFi.status() == WL_CONNECTED) {
+      String lat = "51.6116402309989";
+      String lon = "-0.12889632674983237";
 
+      String finalURL = URL + "lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + ApiKey;
+      Serial.println(finalURL);
+      http.beginRequest();
+      http.get(finalURL);
+      http.endRequest();
 
-    String finalURL = URL + "lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + ApiKey;
-    Serial.println(finalURL);
-    http.beginRequest();
-    http.get(finalURL);
-    http.endRequest();
+      int httpCode = http.responseStatusCode();
+      if (httpCode > 0) {
+        String JSON_Data = http.responseBody();
 
-    int httpCode = http.responseStatusCode();
-    if (httpCode > 0) {
-      String JSON_Data = http.responseBody();
+        Serial.println(JSON_Data);
 
-      Serial.println(JSON_Data);
+        DynamicJsonDocument doc(2048);
+        deserializeJson(doc, JSON_Data);
+        JsonObject obj = doc.as<JsonObject>();
 
-      DynamicJsonDocument doc(2048);
-      deserializeJson(doc, JSON_Data);
-      JsonObject obj = doc.as<JsonObject>();
-
-      const char* description = obj["weather"][0]["description"];
-      const float temp = obj["main"]["temp"];
-      const float humidity = obj["main"]["humidity"];
-
-    
-      Serial.println(description);
-      Serial.println(temp);
-      Serial.println(humidity);
-    } else {
-      Serial.print("Error on HTTP request: ");
-      Serial.println(httpCode);
-    }
-  } else {
-    Serial.println("WiFi not connected!");
-  }
+        const char* description = obj["weather"][0]["description"];
+        const float temp = obj["main"]["temp"];
+        const float humidity = obj["main"]["humidity"];
+        Serial.println(description);
+        Serial.println(temp);
+        Serial.println(humidity);
+      
+      }else {
+        Serial.print("Error on HTTP request: ");
+        Serial.println(httpCode);
+      }
+      } else {
+        Serial.println("WiFi not connected!");
+      }
 }
+    
+  
+  
